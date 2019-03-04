f561fe4b1f2f93eb2d07fb32b0a69f5b5d7f4d1d
Christian Nagel <christian@christiannagel.com>
2019-03-04 12:43:48 +0100

    ASP.NET Core for azure deploy

---
+++ Monday/AzureCloudService1/MyWebRole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyWebRole.MvcApplication" Language="C#" %>
---
+++ Monday/AzureCloudService1/MyWorkerRole/WorkerRole.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+
+namespace MyWorkerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+
+        public override void Run()
+        {
+            Trace.TraceInformation("MyWorkerRole is running");
+
+            try
+            {
+                this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Legen Sie die maximale Anzahl an gleichzeitigen Verbindungen fest.
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Informationen zum Behandeln von Konfigurationsänderungen
+            // finden Sie im MSDN-Thema unter https://go.microsoft.com/fwlink/?LinkId=166357.
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("MyWorkerRole has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("MyWorkerRole is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("MyWorkerRole has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Ersetzen Sie Folgendes durch Ihre eigene Logik.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
