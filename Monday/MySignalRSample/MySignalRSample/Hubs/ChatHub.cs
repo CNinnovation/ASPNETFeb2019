241f8a8df60320c3f3a126f37d9ecbb30aab395d
Christian Nagel <christian@christiannagel.com>
2019-03-04 16:44:40 +0100

    monday samples

---
+++ Monday/MySignalRSample/MySignalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySignalRSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync() => base.OnConnectedAsync();
+
+        public async Task Send(string name, string message)
+        {
+            await base.Clients.All.SendAsync("BroadcastMessage", name, message);
+        }
+    }
+}
---
+++ Monday/OldASPNETApp/OldASPNETApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OldASPNETApp.MvcApplication" Language="C#" %>
